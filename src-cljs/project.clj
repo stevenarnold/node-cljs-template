811a6e9c2ddfdcd6d041545a6d3893c4590c201b
Gary Trakhman <gary.trakhman@gmail.com>
2013-10-26 10:55:25 -0400

    basic systray menu

--- src-cljs/project.clj
+++ src-cljs/project.clj
@@ -4,13 +4,15 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-1934"]]
+                 [org.clojure/clojurescript "0.0-1934"]
+                 ]
 
   :plugins [[lein-cljsbuild "0.3.4"]]
   :cljsbuild {:builds [{;; The path to the top-level ClojureScript source directory:
                         :source-paths ["src"]
+                        :target :nodejs
                         ;; The standard ClojureScript compiler options:
                         ;; (See the ClojureScript compiler documentation for details.)
                         :compiler {:output-to "../src/cljs-app.js"  ; default: target/cljsbuild-main.js
-                                   :optimizations :whitespace
+                                   :optimizations :simple
                                    :pretty-print true}}]})
